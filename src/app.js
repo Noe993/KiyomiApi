5ffa37a9e6bf147004ec01c77f05d9e6d5164c13
Noe <noahs.gapna342@gmail.com>
2024-11-11 18:05:48 -0600

    disque arreglada

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import jugadoresRoutes from './routes/jugadores.routes.js'
+import cors from 'cors' 
 
 const app = express()
+const cors1 = cors()
 
 app.use(express.json())
+app.use(cors1)
 
 app.use('/api',jugadoresRoutes)
 
